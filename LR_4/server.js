98c65a3eccca8b9230ac55f52c611d6de6195c85
Vyacheslav <proninvk_programmer@mail.ru>
2025-06-06 21:56:13 +0300

    LR_4

--- LR_4/server.js
+++ LR_4/server.js
@@ -1,1 +1,14 @@
+const http = require('http');
 
+const HOST = 'localhost';
+const PORT = 8000;
+
+const handler = (req, res) => {
+    res.writeHead(200);
+    res.end('JS is a cool programming language');
+}
+
+const server = http.createServer(handler);
+server.listen(PORT, HOST, () => {
+    console.log(`A server is launched at URL-address http://${HOST}:${PORT}`);
+})
